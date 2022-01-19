7f26a293c969762f02330d5c1c1ebdcd90556ad0
alpagu <alpagu872@gmail.com>
2022-01-19 19:01:10 +0300

    s

---
+++ dom-icerisinden-oge-secimi.js
@@ -0,0 +1,9 @@
+// ********** DOM Icinden Oge Secimi **********
+
+// https://developer.mozilla.org/en-US/docs/Web/API/Document/getElementById
+// https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelector
+
+// let h2 = document.getElementsByTagName('h2')
+let title = document.getElementById('title')
+title.innerHTML = 'denemel'
+console.log(title.innerHTML)

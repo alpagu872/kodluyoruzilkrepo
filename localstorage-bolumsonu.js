e74cc3125fb0b0ce7541ddd7c2a7b91b7571ec46
Alpagu E <83309872+alpagu872@users.noreply.github.com>
2022-01-23 19:15:51 +0300

    localstorage son

--- localstorage-bolumsonu.js
+++ localstorage-bolumsonu.js
@@ -1,4 +1,4 @@
-let counter = localStorage.getItem('counter')
+let counter = localStorage.getItem('counter') ? Number(localStorage.getItem('counter')) : 0
 let counterDOM = document.querySelector(`#counter`)
 let increaseDOM =document.querySelector(`#increase`)
 let decreaseDOM = document.querySelector(`#decrease`)
@@ -8,6 +8,7 @@ counterDOM.innerHTML = counter
 
 function clickEvent(){
     this.id == "increase" ? counter +=1 : counter -=1;
+    localStorage.setItem('counter', counter)
     counterDOM.innerHTML = counter
 }
 
--- localstorage-icerisine-farkli-turde-bilgi-saklamak.js
+++ localstorage-icerisine-farkli-turde-bilgi-saklamak.js
@@ -2,7 +2,7 @@
 
 let user = {userName: "Alpagu", isActive: true}
 
-localStorage.setItem('userInfo', user)
+localStorage.setItem('userInfo', JSON.stringify(user))
 
 let userInfo = localStorage.getItem('userInfo')
 console.log(userInfo)
